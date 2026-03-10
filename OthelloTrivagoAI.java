e24b5d25eebfd9716f937086bea55e589ed0b096
oshe <oshe@itu.dk>
2026-03-10 11:41:36 +0100

    chore: template to write our AI implementation

---
+++ OthelloTrivagoAI.java
@@ -0,0 +1,19 @@
+
+/**
+ * An OthelloAI-implementation. God help us. 
+ * @author Andreas John-Holaus (andjo@itu.dk)
+ * @author Carl Christian David Dam (cacd@itu.dk)
+ * @author Oliver Flinck Sheye (oshe@itu.dk)
+ * @author Philip Bay Quorning (phqu@itu.dk)
+ * @author Rasmus Tornvig Nordquist (ratn@itu.dk)
+ * @version 10.3.2026
+ */
+public class OthelloTrivagoAI implements IOthelloAI {
+
+    @Override
+    public Position decideMove(GameState s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'decideMove'");
+    }
+    
+}
